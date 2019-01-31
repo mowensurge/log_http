587d1bc04cc1b7edf44d7c4ad1bd7c42f4389da7
Mark Owen <mowen132@gmail.com>
2019-01-30 20:30:05 -0700

    Added port flag

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
 func main() {
+	port := flag.Int("port", 8080, "port server listens on")
+	flag.Parse()
+	fmt.Printf("listening on port %d\n", *port)
 	http.HandleFunc("/log", logHandler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
 }
 
 func logHandler(w http.ResponseWriter, r *http.Request) {
